d0f13a73708b700f1f493c83b9c90b32499d2c72
Brian Neal <bneal@primehealthservices.com>
2025-12-09 15:59:45 -0600

    yuck

--- AdventOfCode2025/DayEight.cs
+++ AdventOfCode2025/DayEight.cs
@@ -2,16 +2,20 @@
 
 namespace AdventOfCode2025;
 
-internal class DayEight : Problem
-{
-    private readonly string input;
-    internal DayEight(bool IsTest = false)
-    {
-        Title = IsTest ? "Day 8 Test" : "Day 8 Actual";
-        var rawPath = "./Inputs/DayEight.txt";
-        var filePath = File.Exists(rawPath) ? rawPath : "./Inputs/TEMPLATE.txt";
-        input = IsTest
-            ? """
+internal class DayEight : Problem {
+	private readonly string input;
+	private readonly int connectionThreshold;
+	private readonly Dictionary<JunctionBox, JunctionBox> circuits = new();
+	private readonly Dictionary<JunctionBox, int> ranks = new();
+
+	internal DayEight(bool IsTest = false) {
+		Title = IsTest ? "Day 8 Test" : "Day 8 Actual";
+		connectionThreshold = IsTest ? 10 : 1000;
+
+		var rawPath = "./Inputs/DayEight.txt";
+		var filePath = File.Exists(rawPath) ? rawPath : "./Inputs/TEMPLATE.txt";
+		input = IsTest
+			? """
             162,817,812
             57,618,57
             906,360,560
@@ -33,61 +37,198 @@ internal class DayEight : Problem
             984,92,344
             425,690,689
             """
-            : File.ReadAllText(filePath);
-    }
+			: File.ReadAllText(filePath);
+	}
 
-    internal override string Title { get; init; }
+	internal override string Title { get; init; }
 
-    internal override string SolvePartOne()
-    {
+	internal override string SolvePartOne() {
+		//find minimum spanning tree of junction boxes
+		//KRUSKAL'S ALGORITHM!!! found this searching Udemy graph theory course
 
+		/*
+		 found this on geeksforgeeks, I suspect it will need tweaking
+		 since I am not looping until everything is in one circuit
+		 and because I am not using simple int arrays for edges
 
-        return "NOT IMPLEMENTED";
-    }
+		 public static int KruskalsMST(int V, int[][] edges) {
 
-    internal override string SolvePartTwo()
-    {
-        return "NOT IMPLEMENTED";
-    }
+			Array.Sort(edges, (e1, e2) => e1[2].CompareTo(e2[2]));
+
+			// Traverse edges in sorted order
+			DSU dsu = new DSU(V);
+			int cost = 0, count = 0;
+
+			foreach (var e in edges) {
+				int x = e[0], y = e[1], w = e[2];
+
+				// Make sure that there is no cycle
+				if (dsu.Find(x) != dsu.Find(y)) {
+					dsu.Union(x, y);
+					cost += w;
+					if (++count == V - 1) break;
+				}
+			}
+			return cost;
+		}
+
+		 // Disjoint set union data structure
+		class DSU {
+			private int[] parent, rank;
+
+			public DSU(int n) {
+				parent = new int[n];
+				rank = new int[n];
+				for (int i = 0; i < n; i++) {
+					parent[i] = i;
+					rank[i] = 1;
+				}
+			}
+
+			public int Find(int i) {
+				if (parent[i] != i) {
+					parent[i] = Find(parent[i]);
+				}
+				return parent[i];
+			}
+
+			public void Union(int x, int y) {
+				int s1 = Find(x);
+				int s2 = Find(y);
+				if (s1 != s2) {
+					if (rank[s1] < rank[s2]) {
+						parent[s1] = s2;
+					} else if (rank[s1] > rank[s2]) {
+						parent[s2] = s1;
+					} else {
+						parent[s2] = s1;
+						rank[s1]++;
+					}
+				}
+			}
+		}
+
+		*/
+		var boxes = input.ToJunctionBoxes();
+		var edges = boxes
+			.CalculateAllEdges()
+			.OrderBy(e => e.Weight)
+			.ToList();
+		foreach (var box in boxes) {
+			circuits[box] = box;
+			ranks[box] = 1;
+		}
+		var connectionsMade = 0;
+
+		foreach (var edge in edges) {
+			connectionsMade++;
+			Union(edge.A, edge.B);
+			if (connectionsMade >= connectionThreshold) {
+				break;
+			}
+		}
+
+		return boxes
+			.GroupBy(box => Find(box))
+			.OrderByDescending(g => g.Count())
+			.Take(3)
+			.Aggregate(1, (acc, circuit) => acc *= circuit.Count())
+			.ToString();
+	}
+
+	internal override string SolvePartTwo() {
+		var boxes = input.ToJunctionBoxes();
+		var edges = boxes
+			.CalculateAllEdges()
+			.OrderBy(e => e.Weight)
+			.ToList();
+		foreach (var box in boxes) {
+			circuits[box] = box;
+			ranks[box] = 1;
+		}
+
+		Connection finalConnection = default;
+		foreach (var edge in edges) {
+			Union(edge.A, edge.B);
+			var rootCount = boxes.Select(box => Find(box)).Distinct().Count();
+			
+			if (rootCount == 1) {
+				finalConnection = edge;
+				break;
+			}
+		}
+		
+		return (finalConnection.A.X * finalConnection.B.X).ToString();
+	}
+
+	internal JunctionBox Find(JunctionBox box) {
+
+		if (!circuits.TryGetValue(box, out var found)) {
+			circuits[box] = box;
+			ranks[box] = 1;
+			return box;
+		}
+
+		if (found != box) {
+			circuits[box] = Find(circuits[box]);
+		}
+
+		return circuits[box];
+	}
+
+	internal void Union(JunctionBox A, JunctionBox B) {
+		var circuitA = Find(A);
+		var circuitB = Find(B);
+		if (circuitA != circuitB) {
+			if (ranks[circuitA] < ranks[circuitB]) {
+				circuits[circuitA] = circuitB;
+			} else if (ranks[circuitA] > ranks[circuitB]) {
+				circuits[circuitB] = circuitA;
+			} else {
+				circuits[circuitB] = circuitA;
+				ranks[circuitA]++;
+			}
+		}
+	}
 }
 
-internal record struct Point3D(int X, int Y, int Z);
+// disjoint set union data structure
+// is for cycle detection in kruskal's algorithm
+// It does this by keeping track of connected components
+// and merging them when a new edge is added
+// If an edge connects two nodes that are already in the same component, it creates a cycle
+// and is therefore not added to the MST
+// Makes me wonder if our ClientTree lookups would benefit from this
 
-internal static class DayEightExtensions
-{
-    extension(IEnumerable<Point3D> boxes)
-    {
-        internal (Point3D, Point3D) GetClosestPair()
-        {
-            Point3D closestA = new(0, 0, 0);
-            Point3D closestB = new(0, 0, 0);
-            var closestDistance = double.MaxValue;
-            var junctionBoxes = boxes.ToList();
-            for (int i = 0; i < junctionBoxes.Count; i++)
-            {
-                for (int j = i + 1; j < junctionBoxes.Count; j++)
-                {
-                    var distance = junctionBoxes[i].DistanceTo(junctionBoxes[j]);
-                    if (distance < closestDistance)
-                    {
-                        closestDistance = distance;
-                        closestA = junctionBoxes[i];
-                        closestB = junctionBoxes[j];
-                    }
-                }
-            }
-            return (closestA, closestB);
-        }
-    }
+internal record struct JunctionBox(double X, double Y, double Z);
 
-    extension(Point3D point)
-    {
-        internal double DistanceTo(Point3D other)
-        {
-            var x = point.X - other.X;
-            var y = point.Y - other.Y;
-            var z = point.Z - other.Z;
-            return Math.Sqrt(x * x + y * y + z * z);
-        }
-    }
+internal record struct Connection(JunctionBox A, JunctionBox B, double Weight);
+
+internal static class DayEightExtensions {
+
+	extension(string str) {
+		internal List<JunctionBox> ToJunctionBoxes() =>
+			str.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+				.Select(line => {
+					var parts = line.Split(',');
+					return new JunctionBox(int.Parse(parts[0]), int.Parse(parts[1]), int.Parse(parts[2]));
+				}).ToList();
+	}
+
+	extension(List<JunctionBox> boxes) {
+		internal IEnumerable<Connection> CalculateAllEdges() =>
+			boxes.SelectMany((a, i) =>
+				boxes.Skip(i + 1)
+					.Select(b => new Connection(a, b, a.DistanceTo(b)))
+			);
+	}
+
+	extension(JunctionBox point) {
+		internal double DistanceTo(JunctionBox other) {
+			var x = point.X - other.X;
+			var y = point.Y - other.Y;
+			var z = point.Z - other.Z;
+			return Math.Sqrt(x * x + y * y + z * z);
+		}
+	}
 }
--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -3,14 +3,14 @@ using AdventOfCode2025.Common;
 
 bool useTestData = true;
 List<Problem> problems = [
-    new DayOne(useTestData), new DayOne(),        /* 0:01 */
-    new DayTwo(useTestData), new DayTwo(),        /* 0:03 */
-    new DayThree(useTestData), new DayThree(),    /* 0:01 */
-    new DayFour(useTestData), new DayFour(),      /* 0:08 (with Dict), originally 10+min (Lists and multiple foreaches. I didn't wait to see final time), then 3:57 (Lists and a single for loop)*/
-	new DayFive(useTestData), new DayFive(),      /* 0:01 */
-	new DaySix(useTestData), new DaySix(),        /* 0:01 */
-	new DaySeven(useTestData), new DaySeven(),    /* 0:01 */
-	new DayEight(useTestData), new DayEight(),    /* 0:00 */
+ //   new DayOne(useTestData), new DayOne(),        /* 0:01 */
+ //   new DayTwo(useTestData), new DayTwo(),        /* 0:03 */
+ //   new DayThree(useTestData), new DayThree(),    /* 0:01 */
+ //   new DayFour(useTestData), new DayFour(),      /* 0:08 (with Dict), originally 10+min due to inefficient way of find adjacent spaces, then 3:57 (Lists and a single for loop)*/
+	//new DayFive(useTestData), new DayFive(),      /* 0:01 */
+	//new DaySix(useTestData), new DaySix(),        /* 0:01 */
+	//new DaySeven(useTestData), new DaySeven(),    /* 0:01 */
+	/*new DayEight(useTestData),*/ new DayEight(),    /* 0:01 */
 	//new DayNine(useTestData), new DayNine(),      /* 0:00 */
 	//new DayTen(useTestData), new DayTen(),        /* 0:00 */
 	//new DayEleven(useTestData), new DayEleven(),  /* 0:00 */
@@ -21,5 +21,3 @@ foreach (var problem in problems)
 {
     Console.WriteLine(problem);
 }
-Console.WriteLine("Press any key to close...");
-Console.Read();
